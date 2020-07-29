f3607a66da4b3e21d02da8693c3049c8ae568439
Rohitt Vashishtha <aero31aero@gmail.com>
2020-07-29 19:37:36 +0530

    frontend: Show ugly but working frontend to send message.

--- public/index.js
+++ public/index.js
@@ -6,20 +6,50 @@ const sendParams = {
 };
 
 const main = async () => {
-    const client = await zulip({
+    const z = await zulip({
         username: 'random',
         apiKey: 'random',
         realm: '/proxy', // This should make zulipjs send requests to our express app.
     });
-    const server_info = await client.callEndpoint('/server_settings');
-    const my_info = await client.callEndpoint('/users/me');
+    const server_info = await z.callEndpoint('/server_settings');
+    const my_info = await z.callEndpoint('/users/me');
     console.log(server_info, my_info);
-    window.z = client;
-
-    const handleEvent = async (event) => {
-        console.log('Got Event:', event);
+    window.z = z;
+    return {
+        server: server_info,
+        user: my_info,
     };
-    client.callOnEachEvent(handleEvent, ['message']);
 };
 
-main();
+$(document).ready(async () => {
+    const info = await main();
+    $('#realm .avatar').attr('src', info.server.realm_icon);
+    $('#realm .name').text(info.server.realm_name);
+    $('#realm .url').text(info.server.realm_uri);
+    $('#realm .url').attr('href', info.server.realm_uri);
+    $('#realm .version').text(info.server.zulip_version);
+    $('#user .avatar').attr('src', info.user.avatar_url);
+    $('#user .name').text(info.user.full_name);
+    $('#user .email').text(info.user.email);
+
+    const handleEvent = async (event) => {
+        $('#event-log').append(
+            `======================================\n${JSON.stringify(
+                event,
+                0,
+                4
+            )}\n`
+        );
+    };
+    z.callOnEachEvent(handleEvent, ['message']);
+});
+
+$('#send-message').on('click', () => {
+    const data = {
+        type: 'stream',
+        to: $('#stream').val(),
+        subject: $('#topic').val(),
+        content: $('#content').val(),
+    };
+    z.messages.send(data).then(console.log);
+});
