9df030da14a765ea69ae7577a6b7228f16143024
Rohitt Vashishtha <aero31aero@gmail.com>
2020-07-29 17:36:19 +0530

    express: Print calls from frontend to the console.

--- public/index.js
+++ public/index.js
@@ -1,1 +1,20 @@
 console.log('Hello World');
+
+const sendParams = {
+    to: 'test here',
+    type: 'stream',
+    subject: 'Testing zulip-js',
+    content: 'Something is wrong....',
+};
+
+const main = async () => {
+    const client = await zulip({
+        username: 'random',
+        apiKey: 'random',
+        realm: '/proxy', // This should make zulipjs send requests to our express app.
+    });
+    const reply = await client.messages.send(sendParams);
+    console.log('Reply', reply);
+};
+
+main();
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const formidable = require('express-formidable');
 const zulip = require('./zulip');
 const app = express();
 const port = 3000;
 
-zulip().then((cleint) => {
-    console.log(cleint);
-});
+// zulip().then((client) => {
+//     console.log(client);
+// });
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.raw());
+app.use(formidable());
 
 app.get('/', (req, res) => res.render('index.pug'));
+
+app.post('/proxy/api/v1/*', (req, res) => {
+    console.log(req.path, JSON.stringify(req.fields));
+    res.json({
+        status: 'Success',
+    });
+});
 
 app.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`)
