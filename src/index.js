dabbb7b3ca3797f9221762d95625ae8c8197cf94
Steve Howell <showell@zulip.com>
2020-10-02 11:31:15 +0000

    index.js: Remove zulip logic.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const process = require('process');
 const express = require('express');
-const bodyParser = require('body-parser');
-const formidable = require('express-formidable');
-const zulip = require('./zulip');
 const axios = require('axios');
 const FormData = require('form-data');
 const app = express();
@@ -31,8 +28,8 @@ const client_id = oauth_config.client_id;
 const client_secret = oauth_config.client_secret;
 const redirect_uri = oauth_config.redirect_uri;
 
-zulip().then((client) => {
-    app.get('/', (req, res) => res.render('index.pug'));
+function oauth() {
+    app.get('/', (req, res) => res.send('Use o/code for now'));
 
     app.get('/o/code', (req, res) => {
         const code_url = `${app_url}/o/authorize?response_type=code&client_id=${client_id}&scope=read&redirect_uri=${redirect_uri}`;
@@ -63,25 +60,11 @@ zulip().then((client) => {
         }
     });
 
-    app.all('/proxy/api/v1/*', async (req, res) => {
-        const endpoint = req.path.replace('/proxy/api/v1', '');
-        let data = req.fields;
-        const method = req.method;
-        if (method === 'GET' && req.query) {
-            data = req.query;
-        }
-        const result = await client.callEndpoint(endpoint, method, data);
-        console.log(`zulip: Calling ${method} ${endpoint} => ${result.result}`);
-        res.json(result);
-    });
-
     app.listen(port, () => {
-        console.log(`zulip: Logged in as ${client.config.username}`);
         console.log(`oauth: visit ${host}:${port}/o/code in your browser`);
     });
-});
+}
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+
 app.use(express.static('public'));
-app.use(formidable());
+oauth();
