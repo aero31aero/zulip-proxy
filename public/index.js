a9d9e369979e7e73cd5bbd26f4c67b2073798253
Rohitt Vashishtha <aero31aero@gmail.com>
2020-10-21 00:56:24 +0530

    Show link to the main Zulip server.

--- public/index.js
+++ public/index.js
@@ -1,6 +1,11 @@
 $(document).ready(async () => {
-    const me_div = $('<div>');
-    me_div.text(page_params.me.full_name);
+    const top_div = $('<div>');
+    top_div.text(`${page_params.me.full_name} - `);
+    const link = $('<a>', {
+        text: `Server: ${page_params.app_url}`,
+        href: page_params.app_url,
+    });
+    top_div.append(link);
 
     const pane_config = [
         {
@@ -19,7 +24,7 @@ $(document).ready(async () => {
 
     const main_pane = await window.split_pane.render(pane_config);
 
-    $('#main').append(me_div);
+    $('#main').append(top_div);
     $('#main').append('<hr>');
 
     $('#main').append(main_pane);
