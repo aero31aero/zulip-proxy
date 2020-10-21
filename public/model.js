e65b9dddc3b416fd25dc6f50a9582caafdb69916
Rohitt Vashishtha <aero31aero@gmail.com>
2020-10-22 00:38:14 +0530

    model: Add module and tests.
    Limitations:
    
    1. our structure checks do not check inside arrays yet
       and the code isn't really easy to follow along.
    
    2. we don't exactly support a good way to delete data
       yet; you could delete something and then overwrite
       the complete model to get around this.
    
    Read the tests for understanding the module better.

---
+++ public/model.js
@@ -0,0 +1,74 @@
+window.model = (() => {
+    const base_model = {
+        users: [{}],
+        streams: [{}],
+        messages: [{}],
+        state: {
+            user_id: 1,
+        },
+    };
+
+    let model = {
+        users: [],
+        streams: [],
+        messages: [],
+        state: {
+            user_id: null,
+        },
+    };
+
+    const is_object = (value) => {
+        return typeof value === 'object' && !Array.isArray(value);
+    };
+
+    const has_same_structure = (new_model, base) => {
+        for (const [key, value] of Object.entries(new_model)) {
+            if (base[key]) {
+                const type_check = typeof base[key] === typeof value;
+                const array_check =
+                    Array.isArray(base[key]) === Array.isArray(value);
+                if (!type_check) {
+                    throw new Error(
+                        `Invalid data: "${key}: ${value}" should be ${typeof base[
+                            key
+                        ]} but is ${typeof value}`,
+                        new_model
+                    );
+                } else if (!array_check) {
+                    throw new Error(
+                        `Invalid data: "${key}: ${value}" should ${
+                            Array.isArray(base[key]) ? '' : 'not '
+                        }be an Array`,
+                        new_model
+                    );
+                }
+                if (is_object(value)) {
+                    has_same_structure(value, base[key]);
+                }
+            }
+        }
+    };
+
+    const verify = (new_model) => {
+        if (!is_object(new_model))
+            throw new Error('Invalid data: Expected Object');
+        has_same_structure(new_model, base_model);
+    };
+
+    const main = (new_model, overwrite = false) => {
+        if (new_model) {
+            verify(new_model);
+            if (overwrite) {
+                model = new_model;
+            } else {
+                model = merge(model, new_model);
+            }
+        }
+        return model;
+    };
+    return main;
+})();
+
+if (typeof module !== 'undefined') {
+    module.exports = window.model;
+}
