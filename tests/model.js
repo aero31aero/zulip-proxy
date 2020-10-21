681799aa64c414e81a6da26b1fa04e4f282d58bf
Rohitt Vashishtha <aero31aero@gmail.com>
2020-10-22 00:38:14 +0530

    model: Add module and tests.
    Limitations:
    
    1. our structure checks do not check inside arrays yet
       and the code isn't really easy to follow along.
    
    2. we don't exactly support a good way to delete data
       yet; you could delete something and then overwrite
       the complete model to get around this.
    
    Read the tests for understanding the module better.

---
+++ tests/model.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+global.window = {};
+global.merge = require('deepmerge');
+const model = require('../public/model');
+
+// Get the model.
+let m = model();
+
+assert(typeof m === 'object');
+assert(Array.isArray(m.users));
+
+// Update the model.
+m = model({ streams: [{ id: 1 }] });
+
+assert(m.users.length === 0);
+assert(m.streams[0].id === 1);
+
+// Set the model to this object
+m = model({ streams: [{ id: 2 }] }, true);
+
+assert(m.users === undefined);
+assert(m.streams[0].id === 2);
+
+// Add something to an array, ensure it doesn't wipe existing array.
+m = model({ streams: [{ id: 3, name: 'my_stream' }] });
+
+assert(m.streams.length === 2);
+assert(m.streams[0].id === 2);
+assert(m.streams[1].name === 'my_stream');
+
+assert.throws(
+    () => {
+        // Give invalid data to the model.
+        model('gibberish');
+    },
+    {
+        name: 'Error',
+        message: 'Invalid data: Expected Object',
+    }
+);
+
+console.log('model: Passed tests');
