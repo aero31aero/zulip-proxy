401ca58b2538b36472282d61a8adb151ec579436
Steve Howell <showell@zulip.com>
2020-10-21 19:52:51 +0000

    Extract main view.

--- public/index.js
+++ public/index.js
@@ -13,37 +13,10 @@ $(document).ready(async () => {
         ws: ws,
     });
 
-    const top_div = $('<div>');
-    top_div.text(`${page_params.me.full_name} - `);
-    const link = $('<a>', {
-        text: `Server: ${page_params.app_url}`,
-        href: page_params.app_url,
-    });
-    top_div.append(link);
+    async function redraw_everything() {
+        const main_page = await window.main.render(page_params);
+        $('#main').html(main_page);
+    }
 
-    const pane_config = [
-        {
-            label: 'Tic Tac Toe',
-            view: () => window.tictactoe.render(),
-        },
-        {
-            label: 'Users',
-            view: () => window.users.render(),
-        },
-        {
-            label: 'Messages',
-            view: () => window.messages.render(),
-        },
-        {
-            label: 'Streams',
-            view: () => window.streams.render(),
-        },
-    ];
-
-    const main_pane = await window.split_pane.render(pane_config);
-
-    $('#main').append(top_div);
-    $('#main').append('<hr>');
-
-    $('#main').append(main_pane);
+    redraw_everything();
 });
