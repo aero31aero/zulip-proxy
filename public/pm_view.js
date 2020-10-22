f6c61ebd11da43add45da923aa25ca3c3db4100b
Steve Howell <showell@zulip.com>
2020-10-22 01:45:21 +0000

    Cache user PMs and render more synchronously.
    If we render users and the data is not there
    yet, we return the div right away to our caller,
    and then we call populate only when the data
    gets back.

--- public/pm_view.js
+++ public/pm_view.js
@@ -1,7 +1,9 @@
 window.pm_view = (() => {
     function make(user) {
+        let fetched = false;
+        let message_data;
+
         async function get_message_data(user_id) {
-            // TODO: cache data for user
             const narrow = JSON.stringify([
                 {
                     operator: 'pm-with',
@@ -11,19 +13,39 @@ window.pm_view = (() => {
             const response = await fetch(
                 `/z/messages?num_before=5&anchor=newest&num_after=0&narrow=${narrow}`
             );
-            const message_data = await response.json();
-            return message_data;
+            message_data = await response.json();
+
+            fetched = true;
         }
 
-        async function render() {
-            const data = await get_message_data(user.user_id);
-            const message_table = messages.build_message_table(data.messages);
+        let div;
 
-            const div = $('<div>');
+        function render() {
+            div = $('<div>');
+
+            if (fetched) {
+                console.log('already fetched!');
+                div.html('building');
+                populate(div);
+                return div;
+            }
+
+            get_message_data(user.user_id).then(() => {
+                populate(div);
+            });
+
+            div.html('loading');
+            return div;
+        }
+
+        function populate(div) {
+            const message_table = messages.build_message_table(
+                message_data.messages
+            );
+
+            div.empty();
             div.append(message_table);
             div.append(compose_box.build_for_user(user));
-
-            return div;
         }
 
         return {
