0e7a2193bf6f2b05cd8c72676d95fa6cd0a0bbff
Steve Howell <showell@zulip.com>
2020-10-22 00:53:13 +0000

    Make streams widget sticky.

--- public/streams.js
+++ public/streams.js
@@ -6,18 +6,16 @@ window.streams = (() => {
             };
         }
 
+        const streams = model().streams;
+        const conf = streams.map((stream) => ({
+            label: stream.name,
+            view: build_stream_view(stream),
+        }));
+
+        const pane_widget = split_pane.make(conf, 'streams');
+
         async function render() {
-            const streams = model().streams;
-
-            const conf = streams.map((stream) => ({
-                label: stream.name,
-                view: build_stream_view(stream),
-            }));
-
-            // TODO: persist the widget
-            const pane_widget = split_pane.make(conf, 'streams');
             const pane = await pane_widget.render();
-
             return pane;
         }
 
