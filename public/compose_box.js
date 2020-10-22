df924feeb21a93c00c2581f80dbd33bc5a0f3e1a
Steve Howell <showell@zulip.com>
2020-10-22 01:20:32 +0000

    Extract compose_box.js.

---
+++ public/compose_box.js
@@ -0,0 +1,38 @@
+window.compose_box = (() => {
+    function build_for_user(user) {
+        const div = $('<div>');
+
+        const box = $('<textarea>').val('canned message');
+        const button = $('<button>').text('Send PM');
+
+        button.on('click', async () => {
+            const loader = $('<div>').text('sending...');
+
+            div.append(loader);
+
+            const data = {
+                type: 'private',
+                to: JSON.stringify([user.user_id]),
+                content: box.val(),
+            };
+
+            const response = await fetch('/z/messages', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            loader.text('sent!');
+        });
+
+        div.append(box);
+        div.append(button);
+        return div;
+    }
+
+    return {
+        build_for_user: build_for_user,
+    };
+})();
