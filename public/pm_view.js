e9e6b4e44f686088e1328cfea069a26d1701c76e
Rohitt Vashishtha <aero31aero@gmail.com>
2020-10-27 03:34:08 +0530

    message-list: Scroll to bottom to give a chat-like experience.

--- public/pm_view.js
+++ public/pm_view.js
@@ -6,7 +6,7 @@ window.pm_view = (() => {
         let div;
 
         function render() {
-            div = $('<div>');
+            div = $('<div>').addClass('message-list');
             message_data = _.find_pms_with(user.user_id);
             div.html('building');
             populate(div);
@@ -17,8 +17,8 @@ window.pm_view = (() => {
             const message_table = messages.build_message_table(message_data);
 
             div.empty();
-            div.append(message_table);
             div.append(compose_box.build_for_user(user));
+            div.append(message_table);
         }
 
         return {
