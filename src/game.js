0189d19ec2bd08faf516487ad888be11a5867a0f
Steve Howell <showell@zulip.com>
2020-10-27 15:08:48 +0000

    Queue up events for tictactoe.
    We still only have one active game, but if
    the second player shows up late, they can
    join in a game in progress.

--- src/game.js
+++ src/game.js
@@ -1,13 +1,40 @@
 const WebSocket = require('ws');
 
-exports.handle_message = (clients, client, message) => {
-    payload = {
-        message: JSON.parse(message),
+const game_seq = 101;
+
+const games = {};
+
+games[game_seq] = {
+    events: [],
+};
+
+exports.data = () => {
+    console.info('game info', JSON.stringify(games, null, 4));
+    return games;
+};
+
+exports.handle_message = (clients, client, payload) => {
+    const message = JSON.parse(payload);
+
+    const game_id = message.game_id;
+
+    event = {
+        message: message,
         user_id: client.user_id,
         type: 'tictactoe',
     };
+
+    const game = games[game_id];
+
+    if (!game) {
+        console.warn(`client sent bad game id ${game_id}`);
+        return;
+    }
+
+    game.events.push(event);
+
     clients.forEach((client) => {
-        console.info('about to reply', JSON.stringify(payload));
-        client.ws.send(JSON.stringify(payload));
+        console.info('about to reply', JSON.stringify(event));
+        client.ws.send(JSON.stringify(event));
     });
 };
