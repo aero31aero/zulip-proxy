dd758c796b8186a79c31fa82f016e03e2c013b57
Rohitt Vashishtha <aero31aero@gmail.com>
2020-10-29 04:40:40 +0530

    minor: Remove testing code.

--- public/index.js
+++ public/index.js
@@ -9,25 +9,6 @@ const init_data = async () => {
     await _.fetch_streams();
 };
 
-test_widgets = () => {
-    dumb = widgets.make();
-    smart = widgets.make({
-        should_update: () => window.widgets.PARTIAL,
-        name: 'smart-widget',
-    });
-
-    data = { class: 'hello' };
-    $('body').append(dumb.render(data));
-    $('body').append(smart.render(data));
-    console.log($('#default-widget p').attr('class')); // hello
-    console.log($('#smart-widget p').attr('class')); // hello
-    data = { class: 'world' };
-    dumb.render(data);
-    smart.render(data);
-    console.log($('#default-widget p').attr('class')); // world
-    console.log($('#smart-widget p').attr('class')); // hello world
-};
-
 $(document).ready(async () => {
     const ws_url =
         'ws://' + window.location.hostname + ':' + page_params.ws_port;
@@ -56,6 +37,4 @@ $(document).ready(async () => {
     main_widget = window.main.make(redraw_everything);
 
     redraw_everything();
-
-    // test_widgets();
 });
