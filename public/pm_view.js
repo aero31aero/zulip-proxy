fd4d9149c9c81df48c18f8895ce4d900d508fc17
Steve Howell <showell@zulip.com>
2020-10-28 22:02:49 +0000

    Tweak pm_view.

--- public/pm_view.js
+++ public/pm_view.js
@@ -1,24 +1,22 @@
 window.pm_view = (() => {
     function make(user) {
-        let fetched = false;
-        let message_data;
-
         let div;
+        let message_div;
 
         function render() {
             div = $('<div>').addClass('message-list');
-            message_data = _.find_pms_with(user.user_id);
-            div.html('building');
-            populate(div);
-            return div;
-        }
+            const message_data = _.find_pms_with(user.user_id);
 
-        function populate(div) {
+            message_div = $('<div>');
+
             const message_table = messages.build_message_table(message_data);
+            message_div.append(message_table);
 
             div.empty();
             div.append(compose_box.build_for_user(user));
-            div.append(message_table);
+            div.append(message_div);
+
+            return div;
         }
 
         return {
