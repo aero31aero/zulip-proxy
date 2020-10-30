08121778b4290de9eaf625bb585494a1358677d1
Steve Howell <showell@zulip.com>
2020-10-30 16:40:18 +0000

    Fix misc compose box things and turn off enter-send.
    We reset the focus after sending, and we save
    drafts (which broke for some reason).
    
    I have to turn off enter-send for now, since the
    keyup handler seems to catch stray events from
    nearby buttons if you try to re-focus the compose box.

--- public/compose_box.js
+++ public/compose_box.js
@@ -6,38 +6,47 @@ window.compose_box = (() => {
     function build_for_user(user_id) {
         const div = $('<div>').addClass('compose-box');
 
-        const box = $('<textarea>')
-            .val(drafts.get(user_id) || '')
-            .attr('rows', 4);
+        const draft = drafts.get(user_id) || '';
+
+        const box = $('<textarea>').val(draft).attr('rows', 4);
+
         const send_button = $('<button>').text('Send PM');
 
-        box.on('change', () => {
-            drafts.set(user_id, box.val());
-        });
+        send_button.prop('disabled', !draft);
+
+        function handle_box_keyup(event) {
+            const content = box.val().trim();
+            drafts.set(user_id, content);
+            send_button.prop('disabled', !content);
+        }
+
+        function clear_box() {
+            drafts.delete(user_id);
+            box.off('keyup');
+            box.val('');
+            send_button.prop('disabled', true);
+            box.focus();
+            box.on('keyup', handle_box_keyup);
+        }
 
         const send = () => {
-            const content = box.val();
+            const content = box.val().trim();
             if (content === '') {
                 // we cannot send empty messages;
+                console.warn('did not expect button to be enabled');
                 return;
             }
 
-            box.val('');
-            drafts.delete(user_id);
-
+            clear_box();
             window.transmit.send_pm(user_id, content);
         };
 
         send_button.on('click', send);
-
-        box.on('keyup', (event) => {
-            if (event.keyCode === 13) {
-                send();
-            }
-        });
+        box.on('keyup', handle_box_keyup);
 
         div.append(box);
         div.append(send_button);
+
         return div;
     }
 
