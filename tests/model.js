6a80e51ac80d73bfef19e55278a8802cecf366b9
Rohitt Vashishtha <aero31aero@gmail.com>
2020-11-06 04:56:46 +0530

    model: Nest Users inside Streams as `Streams.subs`.

--- tests/model.js
+++ tests/model.js
@@ -51,4 +51,35 @@ m = model.main({ messages: [{ id: 1, content: 'one-edited' }] });
 assert(m.messages.length === 2);
 assert(m.messages[0].content === 'one-edited');
 
+// Tests for ZulipAccessor based models.
+
+assert(model.Users.list().length === 0);
+model.Users.add({
+    user_id: 1,
+    full_name: 'Test User',
+    email: 'test@example.com',
+});
+let test_user = model.Users.by_id(1);
+// property mappings
+assert(test_user.id === test_user.user_id);
+test_user.id = 2;
+assert(test_user.id === test_user.user_id);
+assert(test_user.name === test_user.full_name);
+test_user.name = 'Testing User';
+assert(test_user.name === test_user.full_name);
+
+assert(model.Streams.list().length === 0);
+model.Streams.add({
+    stream_id: 1,
+    name: 'Test Stream',
+    subscribers: ['test@example.com'],
+});
+let test_stream = model.Streams.by_id(1);
+assert(test_stream.id === test_stream.stream_id);
+test_stream.id = 2;
+assert(test_stream.id === test_stream.stream_id);
+
+// nested properties
+assert(test_stream.subs[0] === test_user);
+
 console.log('model: Passed tests');
