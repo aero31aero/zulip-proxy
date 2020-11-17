a0db926fe81c5bf2da58ffca1e05a99bf267b575
YashRE42 <33805964+YashRE42@users.noreply.github.com>
2020-11-17 21:59:47 +0530

    message-list: Add styling and separate sender name.
    Here we really on classes to apply styles (and
    override user agent styles) so that we do not
    assume the message list is a <ul>.

--- public/messages.js
+++ public/messages.js
@@ -1,12 +1,19 @@
 window.messages = (() => {
     function build_message_table(messages) {
-        const message_ul = $('<ul>');
+        const message_ul = $('<ul>').addClass('message-list-wrapper');
 
-        for (const msg of messages) {
-            const li = $('<li>');
-            li.append($('<b>').text(msg.sender_full_name));
-            li.append($('<br>'));
-            li.append(msg.content);
+        for (let i = 0; i < messages.length; i++) {
+            if (
+                i == 0 ||
+                messages[i - 1].sender_full_name != messages[i].sender_full_name
+            ) {
+                // add names as separate <li>s without repetition
+                const name = $('<li>').addClass('name');
+                name.append($('<b>').text(messages[i].sender_full_name));
+                message_ul.append(name);
+            }
+            const li = $('<li>').addClass('message');
+            li.append(messages[i].content);
             message_ul.append(li);
         }
 
