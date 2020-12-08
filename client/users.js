a67a3c3ff8d6c70b414c9c326c196399840f1df1
Rohitt Vashishtha <aero31aero@gmail.com>
2020-12-09 02:51:41 +0530

    users: Improve user list sorting logic.
    Now we sort the list by 'conversations' instead of just
    showing the recent senders at top.

--- client/users.js
+++ client/users.js
@@ -1,14 +1,17 @@
 window.users = (() => {
-    function get_user_ids_by_recency(show_all = false) {
+    function get_user_ids_by_recency() {
         const user_set = new Set();
         const messages = model.Messages.filter((m) => m.type === 'private');
         for (let i = messages.length - 1; i >= 0; i--) {
-            user_set.add(messages[i].sender_id);
-        }
-        if (show_all) {
-            model.Users.list().forEach((u) => {
-                user_set.add(u.user_id);
-            });
+            const recp = messages[i].display_recipient;
+            if (recp.length === 2) {
+                const other_recipient = recp.find((recipient) => {
+                    return !_.is_me(recipient.id);
+                });
+                user_set.add(other_recipient.id);
+            } else if (recp.length === 1 && _.is_me(recp[0].id)) {
+                user_set.add(recp[0].id);
+            }
         }
         return Array.from(user_set).filter(
             (id) => model.Users.by_id_maybe(id) !== undefined
